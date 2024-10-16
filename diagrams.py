44a942d93cc6f6c4f64c5499cfb5fcdbec7646d3
unknown <wapa9768@gmail.com>
2024-10-16 19:21:22 +0300

    added summary pie chart and region price chart

--- diagrams.py
+++ diagrams.py
@@ -10,9 +10,28 @@ class ShowDiagrams:
         self.df = pd.DataFrame(list(self.fuel_data["average_prices"].items()), columns=["Fuel type", "Average price"])
 
 
+    def plot_summary_pie_chart(self):
+        # Extract the total number of stations and stations without fuel data
+        total_stations = self.fuel_data["summary"]["total_stations"]
+        stations_without_fuel_data = self.fuel_data["summary"]["stations_without_fuel_data"]
+        stations_with_fuel_data = total_stations - stations_without_fuel_data
+
+        # Data for the pie chart
+        labels = ["Asemia joilla on hintatiedot", "Asemia ilman hintatietoja"]
+        sizes = [stations_with_fuel_data, stations_without_fuel_data]
+        colors = ['skyblue', 'orange']
+
+        # Create a pie chart for the summary data
+        plt.figure(figsize=(6, 6))
+        plt.pie(sizes, labels=labels, colors=colors, autopct=lambda pct: f'{int(pct/100 * total_stations)}', 
+        pctdistance=0.80, startangle=90, wedgeprops=dict(width=0.4))
+        plt.title(f'Bensa-asemia yhteensä Suomessa: {total_stations}')
+        plt.tight_layout()
+
+
     def plot_bar_chart(self):
         # Create a bar chart for average prices
-        plt.bar(self.df["Fuel type"], self.df["Average price"], color="skyblue")
+        bars = plt.bar(self.df["Fuel type"], self.df["Average price"], color="skyblue")
         plt.xlabel("Polttoainetyypit")
         plt.ylabel("Keskiverto hinta (€/l)")
         plt.title("Keskiverto hinta eri polttoainetyypeille Suomessa")
@@ -20,6 +39,12 @@ class ShowDiagrams:
         # Set custom labels for the x-axis and rotate them for better readability
         new_labels = ['95', '98', '98+', '85', 'Diesel', 'Diesel+', 'HVO', 'Biokaasu']
         plt.xticks(ticks=range(len(self.df["Fuel type"])), labels=new_labels, rotation=45)
+
+        # Add the exact price values on top of the bars
+        for bar in bars:
+            height = bar.get_height()
+            plt.text(bar.get_x() + bar.get_width() / 2, height - 0.15, f'{height:.3f}', ha='center', va='bottom', color='black')
+
         plt.tight_layout()
 
 
@@ -44,8 +69,8 @@ class ShowDiagrams:
         top_10 = station_data[:10]
         others = station_data[10:]
 
-        # Separate sizes and labels for the top 10
-        if top_10:  # Ensure there are top 10 items
+        # Separate sizes and labels for the top 10, ensure there are top 10 items
+        if top_10:
             top_sizes, top_labels = zip(*top_10)
         else:
             top_sizes, top_labels = [], []
@@ -70,6 +95,40 @@ class ShowDiagrams:
         plt.tight_layout()
 
 
+    def plot_average_region_fuels_prices(self):
+        # Extract average fuel prices by region, define relevant fuel types and new labels
+        region_prices = self.fuel_data["average_fuel_prices_by_region"]
+        regions = list(region_prices.keys())
+        relevant_fuel_types = ["95", "98", "dsl"]
+        new_labels = ["95", "98", "Diesel"]
+
+        # Create a array of prices for the relevant fuel types 
+        prices = np.array([[region_prices[region].get(fuel, 0) for fuel in relevant_fuel_types] for region in regions])
+
+        # Create a bar chart for average prices by region
+        bar_width = 0.15
+        x = np.arange(len(regions))
+        plt.figure(figsize=(14, 8))
+
+        # Plot the bars for each fuel type 
+        for i, fuel in enumerate(relevant_fuel_types):
+            bar = plt.bar(x + i * bar_width, prices[:, i], bar_width, label=fuel)
+
+            # Add the price values on top of the bars
+            for rectangle in bar:
+                height = rectangle.get_height()
+                plt.text(rectangle.get_x() + rectangle.get_width() / 2, height, 
+                f'{height:.3f}', ha='center', va='bottom', color='black', fontsize=6)
+
+        # Set the labels and title
+        plt.xlabel("Maakunnat")
+        plt.ylabel("Keskiverto hinta (€/l)")
+        plt.title("Keskiverto hinta yleisimmille polttoainetyypeille maakunnittain Suomessa")
+        plt.xticks(x + bar_width * (len(relevant_fuel_types) - 1) / 2, labels=regions, rotation=55)
+        plt.legend(title="Polttoainetyypit", labels=new_labels, loc="upper right", bbox_to_anchor=(1.2, 1))
+        plt.tight_layout()
+
+
     def plot_horiz_bar_chart(self):
         # Extract min and max prices for each fuel type
         min_max_prices = self.fuel_data["min_max_prices"]
@@ -85,10 +144,8 @@ class ShowDiagrams:
         y_positions = np.arange(len(fuel_types))
         fig, ax = plt.subplots(figsize=(10, 6))
 
-        # Plot min prices
+        # Plot min and max prices
         min_bars = ax.barh(y_positions - bar_width / 2, min_prices, bar_width, label='Matalin hinta', color='skyblue')
-
-        # Plot max prices
         max_bars = ax.barh(y_positions + bar_width / 2, max_prices, bar_width, label='Korkein hinta', color='orange')
 
         # Add labels to the bars
@@ -118,8 +175,16 @@ class ShowDiagrams:
 # Create an instance of ShowDiagrams with the entire fuel_data object
 visualizer = ShowDiagrams(fuel_data)
 
+# Plot the pie chart for the summary data and show it
+visualizer.plot_summary_pie_chart()
+plt.show()
+
 # Plot the bar chart for average prices and show it
 visualizer.plot_bar_chart()
+plt.show()
+
+# Plot the bar chart for average prices by region and show it
+visualizer.plot_average_region_fuels_prices()
 plt.show()
 
 # Plot the pie chart for the number of gas stations and show it
